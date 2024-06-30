d5493b8938af8fc8dd9b070e9d7a016aca45a41c
PanelIdeas <panelstuff2164261352@outlook.com>
2024-06-29 20:03:24 -0400

    Update ServerDetailsBlock.tsx

--- resources/scripts/components/server/console/ServerDetailsBlock.tsx
+++ resources/scripts/components/server/console/ServerDetailsBlock.tsx
@@ -10,6 +10,8 @@ import { ServerContext } from '@/state/server';
 
 import useWebsocketEvent from '@/plugins/useWebsocketEvent';
 
+import UptimeDuration from '@/components/server/UptimeDuration';
+
 type Stats = Record<'memory' | 'cpu' | 'disk' | 'uptime' | 'rx' | 'tx', number>;
 
 // const getBackgroundColor = (value: number, max: number | null): string | undefined => {
@@ -143,6 +145,48 @@ const ServerDetailsBlock = ({ className }: { className?: string }) => {
             >
                 <StatBlock title={'Storage'}>
                     <Limit limit={textLimits.disk}>{bytesToString(stats.disk)}</Limit>
+                </StatBlock>
+            </div>
+            <div
+                className='transform-gpu skeleton-anim-2'
+                style={{
+                    display: 'flex',
+                    width: '100%',
+                    animationDelay: `225ms`,
+                    animationTimingFunction:
+                        'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
+                }}
+            >
+                <StatBlock title={'Network In'}>
+                    {status === 'offline' ? <span className={'text-gray-400'}>Offline</span> : bytesToString(stats.rx)}
+                </StatBlock>
+            </div>
+            <div
+                className='transform-gpu skeleton-anim-2'
+                style={{
+                    display: 'flex',
+                    width: '100%',
+                    animationDelay: `225ms`,
+                    animationTimingFunction:
+                        'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
+                }}
+            >
+                <StatBlock title={'Network Out'}>
+                    {status === 'offline' ? <span className={'text-gray-400'}>Offline</span> : bytesToString(stats.tx)}
+                </StatBlock>
+            </div>
+            <div
+                className='transform-gpu skeleton-anim-2'
+                style={{
+                    display: 'flex',
+                    width: '100%',
+                    animationDelay: `225ms`,
+                    animationTimingFunction:
+                        'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
+                }}
+            >
+                <StatBlock title={'Uptime'}>
+                    {status === null ? ('Offline') : stats.uptime > 0 ? (<UptimeDuration uptime={stats.uptime / 1000} />) : (capitalize(status))}
                 </StatBlock>
             </div>
         </div>
